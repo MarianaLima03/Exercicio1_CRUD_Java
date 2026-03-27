b8eab4c2f7d5b1c0295b1ceb6286e17266e37fd4
Mariana Lima <marianaml0308@gmail.com>
2026-03-26 21:39:22 -0300

    ex1

--- ex1/src/main/java/com/mml/ex1/controller/LivroController.java
+++ ex1/src/main/java/com/mml/ex1/controller/LivroController.java
@@ -3,34 +3,43 @@ package com.mml.ex1.controller;
 import com.mml.ex1.models.LivroModel;
 import com.mml.ex1.services.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path="/livro")
+@RequestMapping(path = "/livros")
 public class LivroController {
+
     @Autowired
     private LivroService livroService;
 
     @PostMapping
-    public LivroModel criarLivro(@RequestBody LivroModel livroModel) {
-        return livroService.criarLivro(livroModel);
+    public ResponseEntity<LivroModel> criarLivro(@RequestBody LivroModel livroModel) {
+        LivroModel request = livroService.criarLivro(livroModel);
+
+        URI uri = URI.create("/livros/" + request.getId());
+        return ResponseEntity.created(uri).body(request);
     }
 
     @GetMapping
-    public List<LivroModel> listarLivros() {
-        return livroService.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<LivroModel> buscarIdLivro(@PathVariable Long id) {
-        return livroService.buscarIdLivro(id);
+    public ResponseEntity<List<LivroModel>> findAll() {
+        List<LivroModel> request = livroService.findAll();
+        return ResponseEntity.ok().body(request);
     }
 
     @DeleteMapping("/{id}")
-    public void deletarLivro(@PathVariable Long id) {
+    public ResponseEntity<Void> deletarLivro(@PathVariable Long id) {
         livroService.deletarLivro(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<LivroModel>> buscarIdLivro(@PathVariable Long id) {
+        Optional<LivroModel> request = livroService.buscarIdLivro(id);
+        return ResponseEntity.ok().body(request);
     }
 }
--- ex1/src/main/java/com/mml/ex1/models/LivroModel.java
+++ ex1/src/main/java/com/mml/ex1/models/LivroModel.java
@@ -15,6 +15,9 @@ public class LivroModel {
     @Column(name = "ano")
     private Integer anoPublicacao;
 
+    public LivroModel() {
+    }
+
     public Long getId() {
         return id;
     }
